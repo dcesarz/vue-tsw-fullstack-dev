2a9c610337a184537dc4ca36aea4798ae40ba696
Dominika Cesarz <dominikacesarz@yahoo.com>
2020-03-20 10:56:02 +0100

    Dodano zad2 z lab6.

--- lab6/Zad2/zad2.js
+++ lab6/Zad2/zad2.js
@@ -1,9 +1,11 @@
-const groupBy = (tab, key) => {
+const groupBy = (tab, key, fun) => {
     let mmap = new Map();
     let keyTrue = true;
     let keyFalse = false;
     let foo = tab.filter(x => key(x) === true);
+    foo = foo.map(fun);
     let bar = tab.filter(x => key(x) === false);
+    bar = bar.map(fun);
     mmap.set(keyTrue, foo);
     mmap.set(keyFalse, bar);
     return mmap;
@@ -11,4 +13,4 @@ const groupBy = (tab, key) => {
 
 //Quokka pokazuje tu dobry wynik.
 
-console.log(groupBy([3,2,4,4,3], n => n % 2 === 0));
+console.log(groupBy([3,2,4,4,3], n => n % 2 === 0, n => n + 1));
