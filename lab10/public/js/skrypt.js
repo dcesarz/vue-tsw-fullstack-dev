e4c52fbce41b001fc9aaa4f3f0bdff96ed82655b
Dominika Cesarz <dominikacesarz@yahoo.com>
2020-04-27 11:25:06 +0200

    Dodano skrypt.js i index.js, Lab10 zrobione.

--- lab10/public/js/skrypt.js
+++ lab10/public/js/skrypt.js
@@ -1,17 +1,98 @@
 /* globals axios: false */
+/* eslint-disable no-undef */
 // https://github.com/axios/axios
 
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("Tutaj znajdzie się obsługa interfejsu gry!");
+    start();
+});
 
-    // przykład użycia biblioteki axios:
-    axios.post("/mmind", {
-        size: 5,
-        dim: 9,
-        max: 20
-    })
+function start(){
+    document.getElementById("past-guesses").innerHTML = "";
+    let size = parseInt(document.getElementById("size").value);
+    let dim = parseInt(document.getElementById("dim").value);
+    let max = parseInt(document.getElementById("max").value);
+    
+    maxguess = dim;
+
+    for(let i = 1; i < size; i ++){
+        maxguess *= 10;
+        maxguess += dim;
+    }
+
+    axios
+        .post("/mmind", {
+            size: size,
+            dim: dim,
+            max: max,
+        })
         .then((resp) => {
             console.log("Odpowiedź serwera na POST /mmind:");
             console.dir(resp.data);
         });
-});
+    if (max === 0) {
+        max = "no limit";
+    }
+    
+    document.getElementById("guess").setAttribute("max", maxguess);
+    document.getElementById("guess").style.visibility = "visible";
+    document.getElementById("guess-btn").style.visibility = "visible";
+
+    document.getElementById("bscore").innerHTML = 0;
+    document.getElementById("wscore").innerHTML = 0;
+    document.getElementById("unrated").innerHTML = size;
+    document.getElementById("guesses-left").innerHTML = max;
+    document.getElementById("past-guesses").innerHTML = "empty";
+
+}
+
+function guess() {
+    let guess = document.getElementById("guess").value;
+    let bscore = parseInt(document.getElementById("bscore").innerHTML);
+    let wscore = parseInt(document.getElementById("wscore").innerHTML);
+    let remaining = parseInt(document.getElementById("unrated").innerHTML);
+
+    let newGuess = document.createElement("div");
+    let headerBig = document.createElement("h3");
+    let headerSmall = document.createElement("h4");
+
+
+    axios
+        .patch("/mmind", {
+            guess: guess,
+        })
+        .then((resp) => {
+            let black = resp.data.black;
+            let white = resp.data.white;
+            
+            headerSmall.innerHTML = "Black: " + black + " &emsp; White: " + white;
+            document.getElementById("bscore").innerHTML = bscore + black;
+            document.getElementById("wscore").innerHTML = wscore + white;
+
+            remaining = remaining - (black + white);
+            document.getElementById("unrated").innerHTML = remaining;
+            
+            if (remaining <= 0) {
+                document.getElementById("guess").style.visibility = "hidden";
+                document.getElementById("guess-btn").style.visibility = "hidden";
+            }
+        });
+
+    headerBig.innerHTML = "Guess: " + guess;
+    newGuess.className = "log-item";
+    newGuess.appendChild(headerBig);
+    newGuess.appendChild(headerSmall);
+
+    document.getElementById("past-guesses").innerHTML = "";
+    document.getElementById("past-guesses").prepend(newGuess);
+
+    let guessesLeft = document.getElementById("guesses-left").innerHTML;
+
+    if (!isNaN(guessesLeft)) {
+        let gl = guessesLeft - 1;
+        document.getElementById("guesses-left").innerHTML = gl;
+        if (gl <= 0) {
+            document.getElementById("guess").style.visibility = "hidden";
+            document.getElementById("guess-btn").style.visibility = "hidden";
+        }
+    }
+}
--- lab10/routes/index.js
+++ lab10/routes/index.js
@@ -1,28 +1,45 @@
 "use strict";
 
-// Sugestia – funkcję oceniającą ruchy najlepiej
-// umieścić w osobnym module, a poniżej jedynie z niej
-// skorzystać.
-
+const uuid = require("uuidv4").uuid;
+const rtng = require("./rating");
 const express = require("express");
 const router = express.Router();
 
 router.route("/")
     .post((req, res) => {
         let params = req.body;
-        // tworzymy nową grę
+        req.session.id = uuid();
+        let answer = "";
+        let rated = [];
+        for (let i = 0; i < params.size; i++) {
+            answer += getRandomInt(params.dim).toString();
+            rated[i] = false;
+        }
+        req.session.answer = answer;
+        req.session.rated = rated;
         res.json({
-            msg: "nowa gra",
+            msg: "new game",
             params
         });
     })
     .patch((req, res) => {
-        let ruch = req.body;
+        let move = rtng.rating(
+            req.body.guess,
+            req.session.rated,
+            req.session.answer
+        );
+        req.session.rated = move[0];
         // oceniamy ruch
         res.json({
-            msg: "ocena ruchu",
-            ruch
+            black: move[1],
+            white: move[2]
         });
     });
 
 module.exports = router;
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+  
