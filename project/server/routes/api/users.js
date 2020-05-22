725d87cc3be9a764faa0f4b42a010bc06708488c
Dominika Cesarz <dominikacesarz@yahoo.com>
2020-05-22 23:41:25 +0200

    First project commit. Basics.

---
+++ project/server/routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//get add del upd
+router.get('/', (req, res) =>{
+    
+})
+
+
+module.exports = router;
