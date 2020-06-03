caad7e565627d60a9e93741cb12333dffed454f5
Dominika Cesarz <dominikacesarz@yahoo.com>
2020-06-03 20:10:58 +0200

    deleted the old project folder to avoid confusion that has already occured

--- project/https/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    const fs = require("fs");
-    const https = require("https");
-    return https.createServer({
-        rejectUnauthorized: false,
-        key: fs.readFileSync("./https/my.key"),
-        cert: fs.readFileSync("./https/my.crt")
-    }, app);
-};
